0e1a543ed9f427e2f35e22df20eeafdf3a66de00
CanToon <lemanhloc@gmail.com>
2023-05-15 16:16:08 +0700

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,22 +21,30 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const controller = new AbortController()
 
+    setIsLoading(true)
     apiClient
       .get<FetchGamesResponse>('https://api.rawg.io/api/games', {
         signal: controller.signal,
       })
-      .then(res => setGames(res.data.results))
-      .catch(err =>
-        err instanceof CanceledError ? setError('') : setError(err.message)
-      )
+      .then(res => {
+        setIsLoading(false)
+        setGames(res.data.results)
+      })
+
+      .catch(err => {
+        if (err instanceof CanceledError) return
+        setError(err.message)
+        setIsLoading(false)
+      })
     return () => controller.abort()
   }, [])
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
